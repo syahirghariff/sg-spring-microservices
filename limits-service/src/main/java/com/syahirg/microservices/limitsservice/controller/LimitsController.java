1179469f1a7b574863f202339eb098cd05ec3900
syahirghariff <syahirghariff@gmail.com>
2024-04-13 16:41:47 +0800

    get configuration from properties file

--- limits-service/src/main/java/com/syahirg/microservices/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/syahirg/microservices/limitsservice/controller/LimitsController.java
@@ -1,15 +1,22 @@
 package com.syahirg.microservices.limitsservice.controller;
 
 import com.syahirg.microservices.limitsservice.bean.Limits;
+import com.syahirg.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
+    private final Configuration configuration;
+    public LimitsController(Configuration configuration){
+        this.configuration = configuration;
+    }
 
     @GetMapping("/limits")
     public Limits retrieveLimits(){
 
-        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(),
+                configuration.getMaximum());
     }
 }
