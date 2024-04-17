05ad02d7323495051b0558f7ad5a2400d9828942
syahirghariff <syahirghariff@gmail.com>
2024-04-17 22:17:02 +0800

    setting up dynamic port in the response:

--- currency-exchange-service/src/main/java/com/syahirg/microservices/currencyexchangeservice/model/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/syahirg/microservices/currencyexchangeservice/model/CurrencyExchange.java
@@ -14,4 +14,12 @@ public class CurrencyExchange {
     private String from;
     private String to;
     private BigDecimal conversionMultiple;
+    private String environment;
+
+    public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
 }
