4e44a37d37ff643d7f021482248e6e22d0301037
syahirghariff <syahirghariff@gmail.com>
2024-04-17 23:36:44 +0800

    configure jpa and initialized data

--- currency-exchange-service/src/main/java/com/syahirg/microservices/currencyexchangeservice/model/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/syahirg/microservices/currencyexchangeservice/model/CurrencyExchange.java
@@ -1,5 +1,8 @@
 package com.syahirg.microservices.currencyexchangeservice.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +12,16 @@ import java.math.BigDecimal;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class CurrencyExchange {
+
+    @Id
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+values(10001, 'USD', 'MYR', 65, '');
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+values(10002, 'EUR', 'MYR', 75, '');
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+values(10003, 'AUD', 'MYR', 85, '');
