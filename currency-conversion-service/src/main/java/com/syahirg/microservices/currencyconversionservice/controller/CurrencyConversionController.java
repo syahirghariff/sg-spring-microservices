34b40295ff8f91e9aa5d0e829aba7d531fe890d9
syahirghariff <syahirghariff@gmail.com>
2024-04-18 10:58:38 +0800

    using feign rest client

--- currency-conversion-service/src/main/java/com/syahirg/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/syahirg/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,6 +1,8 @@
 package com.syahirg.microservices.currencyconversionservice.controller;
 
 import com.syahirg.microservices.currencyconversionservice.model.CurrencyConversion;
+import com.syahirg.microservices.currencyconversionservice.proxy.CurrencyExchangeProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,9 @@ import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeProxy proxy;
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculate(
@@ -29,6 +34,23 @@ public class CurrencyConversionController {
                 uriVariables);
 
         CurrencyConversion currencyConversion = responseEntity.getBody();
+        return new CurrencyConversion(currencyConversion.getId(),
+                from,
+                to,
+                quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment());
+    }
+
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculate2(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity){
+
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
         return new CurrencyConversion(currencyConversion.getId(),
                 from,
                 to,
---
+++ currency-conversion-service/src/main/java/com/syahirg/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,15 @@
+package com.syahirg.microservices.currencyconversionservice.proxy;
+
+import com.syahirg.microservices.currencyconversionservice.model.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="currency-exchange", url="localhost:8000")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(
+            @PathVariable String from,
+            @PathVariable String to);
+}
