afda9fe879f8df86eb0a231d92f79fb65a902f11
syahirghariff <syahirghariff@gmail.com>
2024-04-18 11:05:37 +0800

    naming server

--- currency-conversion-service/src/main/java/com/syahirg/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/syahirg/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange", url="localhost:8000")
+@FeignClient(name="currency-exchange", url="localhost:8000; localhost:8001")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
