317c6744c026277e6dbe01655164939709390635
syahirghariff <syahirghariff@gmail.com>
2024-07-06 06:51:17 +0800

    circuit breaker features

--- currency-exchange-service/src/main/java/com/syahirg/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/syahirg/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.syahirg.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,8 @@ public class CircuitBreakerController {
 
 
     @GetMapping("/sample-api")
-    @Retry(name="sample-api", fallbackMethod = "myResponse" )
+    //@Retry(name="sample-api", fallbackMethod = "myResponse" )
+    @CircuitBreaker(name="default", fallbackMethod = "myResponse")
     public String sampleApi(){
         log.info("sample api call received");
         ResponseEntity<String> entity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy", String.class);
