e0a7554358ffcc43b53d836bab2e81efce1455a4
syahirghariff <syahirghariff@gmail.com>
2024-07-05 11:41:23 +0800

    Resillience4j and fallback methods

---
+++ currency-exchange-service/src/main/java/com/syahirg/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -0,0 +1,26 @@
+package com.syahirg.microservices.currencyexchangeservice.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class CircuitBreakerController {
+
+
+    @GetMapping("/sample-api")
+    @Retry(name="sample-api", fallbackMethod = "myResponse" )
+    public String sampleApi(){
+        log.info("sample api call received");
+        ResponseEntity<String> entity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy", String.class);
+        return entity.getBody();
+    }
+
+    public String myResponse(Exception exception){
+        return "fallback-response";
+    }
+}
