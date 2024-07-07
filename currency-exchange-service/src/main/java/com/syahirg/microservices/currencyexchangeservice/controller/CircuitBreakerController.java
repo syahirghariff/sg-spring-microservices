e81ae2077906363cfd812f44eedee8f658393e37
syahirghariff <syahirghariff@gmail.com>
2024-07-07 14:57:22 +0800

    resillience4j rate limiting and bulkhead

--- currency-exchange-service/src/main/java/com/syahirg/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/syahirg/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.syahirg.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,20 @@ public class CircuitBreakerController {
         log.info("sample api call received");
         ResponseEntity<String> entity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy", String.class);
         return entity.getBody();
+    }
+
+    @GetMapping("/sample-api2")
+    @RateLimiter(name="default")
+    public String sampleApi2(){
+        log.info("sample api call received");
+        return "sample-api2";
+    }
+
+    @GetMapping("/sample-api3")
+    @Bulkhead(name="default")
+    public String sampleApi3(){
+        log.info("sample api call received");
+        return "sample-api3";
     }
 
     public String myResponse(Exception exception){
